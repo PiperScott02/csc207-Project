87c92e2c6a92688274f8cac8869850c52d3589a7
Hana Cabangon <cabangon.hana@gmail.com>
2026-08-07 11:50:08 -0400

    Update DeleteHoldingPresenter to update LoggedInViewModel state

--- src/main/java/interface_adapter/delete_holding/DeleteHoldingPresenter.java
+++ src/main/java/interface_adapter/delete_holding/DeleteHoldingPresenter.java
@@ -1,13 +1,27 @@
 package interface_adapter.delete_holding;
 
+import interface_adapter.logged_in.LoggedInState;
+import interface_adapter.logged_in.LoggedInViewModel;
 import use_case.delete_holding.DeleteHoldingOutputBoundary;
 import use_case.delete_holding.DeleteHoldingOutputData;
 
 public class DeleteHoldingPresenter implements DeleteHoldingOutputBoundary {
 
+    private final LoggedInViewModel loggedInViewModel;
+
+    public DeleteHoldingPresenter(LoggedInViewModel loggedInViewModel) {
+        this.loggedInViewModel = loggedInViewModel;
+    }
+
     @Override
     public void prepareSuccessView(DeleteHoldingOutputData outputData) {
-        // Here you update your ViewModel or state with the updated portfolio
+        LoggedInState currentState = loggedInViewModel.getState();
+
+        currentState.setHoldings(outputData.getPortfolio().getHoldings());
+
+        loggedInViewModel.setState(currentState);
+        loggedInViewModel.firePropertyChanged("state");
+
         System.out.println("Holding deleted successfully!");
     }
 
