d21f8ac372d34d7e71387b3dbddd87e8d9a7df40
diyakozahir <zahirdiyako@gmail.com>
2026-08-08 14:42:02 -0400

    refactor ticker search DAO

--- src/main/java/data_access/ticker_search/TickerSearchDataAccessObject.java
+++ src/main/java/data_access/ticker_search/TickerSearchDataAccessObject.java
@@ -1,73 +1,25 @@
 package data_access.ticker_search;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import entity.Stock;
 import use_case.TickerSearchDataAccessInterface;
 
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.util.concurrent.TimeUnit;
 
 public class TickerSearchDataAccessObject implements TickerSearchDataAccessInterface {
-    private static final String function = "OVERVIEW";
-    private static final HttpClient client = HttpClient.newBuilder().build();
-    private static final Gson GSON = new GsonBuilder()
-            .setPrettyPrinting()
-            .create();
-
-    private final String api_key;
     private final TickerSearchDailyDataAccessObject tickerSearchDailyDataAccessObject;
+    private final TickerSearchOverviewDataAccessObject tickerSearchOverviewDataAccessObject;
 
-    public TickerSearchDataAccessObject(String api_key) {
-        this.api_key = api_key;
-        this.tickerSearchDailyDataAccessObject = new TickerSearchDailyDataAccessObject(this.api_key);
+    public TickerSearchDataAccessObject(String apiKey) {
+        this.tickerSearchDailyDataAccessObject = new TickerSearchDailyDataAccessObject(apiKey);
+        this.tickerSearchOverviewDataAccessObject = new TickerSearchOverviewDataAccessObject(apiKey);
     }
 
     @Override
     public Stock createBasicStock(String tickerSymbol) throws IOException, InterruptedException {
-        final String query = "?function=" + function +
-                "&symbol=" + tickerSymbol +
-                "&apikey=" + api_key;
-        final String location = "https://www.alphavantage.co/query" + query;
-
-        HttpRequest request = HttpRequest
-                .newBuilder()
-                .uri(URI.create(location))
-                .build();
-        TimeUnit.SECONDS.sleep(1);
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-
-        // AlphaVantage API does not return OVERVIEW information for all stocks
-        if (response.body().equals("{}")) {
-            return tickerSearchDailyDataAccessObject.createBasicStock(tickerSymbol);
-        }
-        final Stock stock = parseJSON(response.body());
+        Stock stock = this.tickerSearchOverviewDataAccessObject.createBasicStock(tickerSymbol);
         if (stock == null) {
-            return tickerSearchDailyDataAccessObject.createBasicStock(tickerSymbol);
+            stock = this.tickerSearchDailyDataAccessObject.createBasicStock(tickerSymbol);
         }
-        return stock;
-    }
-
-    private Stock parseJSON(String responseBody) {
-        TickerSearchJSONResponse javaResponse =
-                GSON.fromJson(responseBody, TickerSearchJSONResponse.class);
-
-        if (javaResponse.getStockPrice() == null) {
-            return null;
-        }
-
-        final Stock stock = new Stock();
-
-        stock.setTickerSymbol(javaResponse.getTickerSymbol());
-        stock.setCompanyName(javaResponse.getCompanyName());
-        stock.setPreviousClose(javaResponse.getStockPrice());
-        stock.setCountry(javaResponse.getCountry());
-        stock.setIndustry(javaResponse.getIndustry());
-
         return stock;
     }
 }
---
+++ src/main/java/data_access/ticker_search/TickerSearchOverviewDataAccessObject.java
@@ -0,0 +1,73 @@
+package data_access.ticker_search;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import entity.Stock;
+import use_case.TickerSearchDataAccessInterface;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.concurrent.TimeUnit;
+
+public class TickerSearchOverviewDataAccessObject implements TickerSearchDataAccessInterface {
+    private static final String function = "OVERVIEW";
+    private static final HttpClient client = HttpClient.newBuilder().build();
+    private static final Gson GSON = new GsonBuilder()
+            .setPrettyPrinting()
+            .create();
+
+    private final String api_key;
+    private final TickerSearchDailyDataAccessObject tickerSearchDailyDataAccessObject;
+
+    public TickerSearchOverviewDataAccessObject(String api_key) {
+        this.api_key = api_key;
+        this.tickerSearchDailyDataAccessObject = new TickerSearchDailyDataAccessObject(this.api_key);
+    }
+
+    @Override
+    public Stock createBasicStock(String tickerSymbol) throws IOException, InterruptedException {
+        final String query = "?function=" + function +
+                "&symbol=" + tickerSymbol +
+                "&apikey=" + api_key;
+        final String location = "https://www.alphavantage.co/query" + query;
+
+        HttpRequest request = HttpRequest
+                .newBuilder()
+                .uri(URI.create(location))
+                .build();
+        TimeUnit.SECONDS.sleep(1);
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        // AlphaVantage API does not return OVERVIEW information for all stocks
+        if (response.body().equals("{}")) {
+            return tickerSearchDailyDataAccessObject.createBasicStock(tickerSymbol);
+        }
+        final Stock stock = parseJSON(response.body());
+        if (stock == null) {
+            return tickerSearchDailyDataAccessObject.createBasicStock(tickerSymbol);
+        }
+        return stock;
+    }
+
+    private Stock parseJSON(String responseBody) {
+        TickerSearchOverviewJSONResponse javaResponse =
+                GSON.fromJson(responseBody, TickerSearchOverviewJSONResponse.class);
+
+        if (javaResponse.getStockPrice() == null) {
+            return null;
+        }
+
+        final Stock stock = new Stock();
+
+        stock.setTickerSymbol(javaResponse.getTickerSymbol());
+        stock.setCompanyName(javaResponse.getCompanyName());
+        stock.setPreviousClose(javaResponse.getStockPrice());
+        stock.setCountry(javaResponse.getCountry());
+        stock.setIndustry(javaResponse.getIndustry());
+
+        return stock;
+    }
+}
