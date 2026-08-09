e95bf246c39a2ad77695a9e0d4f311f03b435afa
Hana Cabangon <cabangon.hana@gmail.com>
2026-08-09 08:47:13 -0400

    Remove api key

--- src/main/java/app/PortfolioPilotMain.java
+++ src/main/java/app/PortfolioPilotMain.java
@@ -107,7 +107,7 @@ public final class PortfolioPilotMain {
         /*
          * Alpha Vantage API key
          */
-        final String apiKey = "NKH8SNZW8I690AJQ";
+        final String apiKey = "INSERT_API_KEY";
 
         // ==========================================
         // 2. Data Access Objects
@@ -237,7 +237,8 @@ public final class PortfolioPilotMain {
                 PortfolioHealthUseCaseFactory.create(
                         viewManagerModel,
                         portfolioHealthViewModel,
-                        loggedInViewModel
+                        loggedInViewModel,
+                        portfolioHealthController
                 );
         views.add(portfolioHealthView, portfolioHealthView.viewName);
 
